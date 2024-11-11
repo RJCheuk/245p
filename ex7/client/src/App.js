6610c94781351535e008fbad8bf0277fdac70394
jiachengzhuo <richardjcheuk@gmail.com>
2024-11-10 23:05:14 -0800

    Add ex7 fold to the repo

---
+++ ex7/client/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+// App.js
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Sidebar />
+      <div className="main-content">
+        <h1>Welcome to the Website!</h1>
+        <p>This is the main content area where your content will go.</p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
