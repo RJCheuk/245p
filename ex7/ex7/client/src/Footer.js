51f864457704da4858de852dd80db92782de7c12
jiachengzhuo <richardjcheuk@gmail.com>
2024-11-12 16:06:12 -0800

    Commit your changes

---
+++ ex7/ex7/client/src/Footer.js
@@ -0,0 +1,12 @@
+// Footer.js
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>© 2024 CompanyName. All rights reserved.</p>
+    </div>
+  );
+}
+
+export default Footer;
